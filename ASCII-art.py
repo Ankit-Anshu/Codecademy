5cf8a703330c42d2d07d8a9ed9d3540ca359439c
Ankit Anshu <46862749+Ankit-Anshu@users.noreply.github.com>
2021-10-26 11:20:25 +0530

    Create ASCII-art.py

---
+++ ASCII-art.py
@@ -0,0 +1,12 @@
+# Ankit
+
+Name = """ 
+  A       N   N   K   K    IIIII    TTTTT
+ A A      NN  N   K  K       I        T
+A   A     N N N   K K        I        T
+AAAAA     N  NN   Kk         I        T
+A   A     N   N   K K        I        T
+A   A     N   N   K  K       I        T
+A   A     N   N   K   K    IIIII      T
+"""
+print(Name)
